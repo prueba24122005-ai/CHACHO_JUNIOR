ed50f38818dbb7fba37524867a0c914bebe502f9
prueba24122005-ai <prueba24122005@gmail.com>
2026-03-10 20:32:48 +0100

    Cambio Nombre Variables
    -Cambie el nombre de las variables para que se mas legibles 
    -Acomode el scanner para que aparezca al inicio y no haga tanto bulto al final del código.

--- README.java
+++ README.java
@@ -3,25 +3,27 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        double[] p = {10.5, 20.0, 5.75, 100.0};
-        int[] c = {2, 1, 5, 1};
-        int[] t = {1, 2, 1, 2}
+        Scanner sc = new Scanner(System.in);
+        double[] precios = {10.5, 20.0, 5.75, 100.0};
+        int[] cantidades = {2, 1, 5, 1};
+        int[] tipos = {1, 2, 1, 2}
         
-        double tt = 0;
+        double totalFinal = 0;
 
-        for (int i = 0; i < p.length; i++) {
-            double sub = p[i] * c[i];
+        for (int i = 0; i < precios.length; i++) {
+            double sub = precios[i] * cantidades[i];
             double imp = 0;
 
-            if (t[i] == 1) {
-                imp = sub * 0.04;
-            } else if (t[i] == 2) {
-                imp = sub * 0.21;
+            if (tipos[i] == 1) {
+                imp = sub * 0.04; // IVA Superreducido
+            } else if (tipos[i] == 2) {
+                imp = sub * 0.21;// IVA General
             }
-
+       
             double res = sub + imp;
             
-            if (c[i] > 3) {
+            // Descuento si la cantidad es grande
+            if (cantidades[i] > 3) {
                 res = res - (res * 0.1);
             }
 
@@ -30,12 +32,12 @@ public class Main {
         }
 
         System.out.println("TOTAL FINAL: " + tt);
-        
-        Scanner sc = new Scanner(System.in);
+       
         System.out.println("¿Confirmar pedido? (s/n)");
         String s = sc.nextLine();
         if (s.equals("s")) {
             System.out.println("Guardando...");
         }
     }
+
 }
