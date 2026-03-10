ef76cc6c300700bda3ec12e6f94012ad7eb0f174
prueba24122005-ai <prueba24122005@gmail.com>
2026-03-10 20:08:42 +0100

    Fix syntax error and clean up comments in README.java
    Quitando los comentarios

--- README.java
+++ README.java
@@ -3,27 +3,24 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        // Datos de entrada: precio, cantidad, tipo de producto (1: Alimento, 2: Electronica)
         double[] p = {10.5, 20.0, 5.75, 100.0};
         int[] c = {2, 1, 5, 1};
-        int[] t = {1, 2, 1, 2};
-        //maldita sea
+        int[] t = {1, 2, 1, 2}
+        
         double tt = 0;
 
         for (int i = 0; i < p.length; i++) {
             double sub = p[i] * c[i];
             double imp = 0;
 
-            // Calcular impuesto segun tipo
             if (t[i] == 1) {
-                imp = sub * 0.04; // IVA Superreducido
+                imp = sub * 0.04;
             } else if (t[i] == 2) {
-                imp = sub * 0.21; // IVA General
+                imp = sub * 0.21;
             }
 
             double res = sub + imp;
             
-            // Descuento si la cantidad es grande
             if (c[i] > 3) {
                 res = res - (res * 0.1);
             }
@@ -34,7 +31,6 @@ public class Main {
 
         System.out.println("TOTAL FINAL: " + tt);
         
-        // Parte interactiva innecesariamente mezclada
         Scanner sc = new Scanner(System.in);
         System.out.println("¿Confirmar pedido? (s/n)");
         String s = sc.nextLine();
