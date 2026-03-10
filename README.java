cffafc76c11f7e5e147f9d5980dc02f727cbe82a
sergiogarcia060607-lab <sergiogarcia060607@gmail.com>
2026-03-10 20:29:15 +0100

    Update README.java
    He borrado todo para separlo todo con metodos. Los metodos se llamaran confirmarPedido , calcularTotal , calcularIVA , aplicarDescuento.

--- README.java
+++ README.java
@@ -3,43 +3,20 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        // Datos de entrada: precio, cantidad, tipo de producto (1: Alimento, 2: Electronica)
-        double[] p = {10.5, 20.0, 5.75, 100.0};
-        int[] c = {2, 1, 5, 1};
-        int[] t = {1, 2, 1, 2};
-        //maldita sea
-        double tt = 0;
+        public class Sergio_Parte {
+    public static void main(String[] args) {
+         double[] precios = {10.5, 20.0, 5.75, 100.0};
+        int[] cantidades = {2, 1, 5, 1};
+        int[] tipos = {1, 2, 1, 2};
 
-        for (int i = 0; i < p.length; i++) {
-            double sub = p[i] * c[i];
-            double imp = 0;
+        double totalFinal = calcularTotal(precios, cantidades, tipos);
 
-            // Calcular impuesto segun tipo
-            if (t[i] == 1) {
-                imp = sub * 0.04; // IVA Superreducido
-            } else if (t[i] == 2) {
-                imp = sub * 0.21; // IVA General
-            }
+        System.out.println("TOTAL FINAL: " + totalFinal);
 
-            double res = sub + imp;
-            
-            // Descuento si la cantidad es grande
-            if (c[i] > 3) {
-                res = res - (res * 0.1);
-            }
-
-            System.out.println("Item " + i + ": " + res);
-            tt += res;
-        }
-
-        System.out.println("TOTAL FINAL: " + tt);
-        
-        // Parte interactiva innecesariamente mezclada
-        Scanner sc = new Scanner(System.in);
-        System.out.println("¿Confirmar pedido? (s/n)");
-        String s = sc.nextLine();
-        if (s.equals("s")) {
-            System.out.println("Guardando...");
-        }
+        confirmarPedido();
+   
+    
     }
+    
+}
 }
