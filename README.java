d748d2e813f65720d587d57f14267b01272b1f0f
sergiogarcia060607-lab <sergiogarcia060607@gmail.com>
2026-03-10 20:32:18 +0100

    Añadir nuevo metodo calcular total.
    He añadido un nuevo metodo que incluye un bucle for que recorre todos los elementos de los arrays de precios cantidades y tipos, para luego devolver el total final.

--- README.java
+++ README.java
@@ -1,9 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Proyecto_Chacho;
+
 import java.util.Scanner;
 
-
-public class Main {
-    public static void main(String[] args) {
-        public class Sergio_Parte {
+/**
+ *
+ * @author WEB1-19
+ */
+public class Sergio_Parte {
     public static void main(String[] args) {
          double[] precios = {10.5, 20.0, 5.75, 100.0};
         int[] cantidades = {2, 1, 5, 1};
@@ -17,6 +24,19 @@ public class Main {
    
     
     }
-    
-}
+    public static double calcularTotal(double[] precios, int[] cantidades, int[] tipos) {
+        double totalFinal = 0;
+
+        for (int i = 0; i < precios.length; i++) {
+            double subtotal = precios[i] * cantidades[i];
+            double impuesto = calcularIVA(subtotal, tipos[i]);
+            double precioFinal = subtotal + impuesto;
+            precioFinal = aplicarDescuento(precioFinal, cantidades[i]);
+
+            System.out.println("Item " + i + ": " + precioFinal);
+            totalFinal += precioFinal;
+        }
+
+        return totalFinal;
+    }
 }
