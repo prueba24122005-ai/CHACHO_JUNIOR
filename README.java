23daed327ff4e50a65a4507c98b4dc771d34c72f
ikerbrac <ikerbrac@gmail.com>
2026-03-15 20:01:45 +0100

    Cambios en main y nuevo método
    - El main ahora gestiona la confirmación del pedido de forma aislada.
    - He creado el método procesarPedido que realiza todo el cálculo sin imprimir nada en consola.

--- README.java
+++ README.java
@@ -6,40 +6,40 @@ public class Main {
         int[] cantidades = {2, 1, 5, 1};
         int[] tipos = {1, 2, 1, 2};
 
+        double totalFinal = procesarPedido(precios, cantidades, tipos);
+        System.out.println("TOTAL FINAL: " + totalFinal);
+
+        gestionarConfirmacion();
+    }
+
+    public static double procesarPedido(double[] precios, int[] cantidades, int[] tipos) {
         double totalFinal = 0;
         for (int i = 0; i < precios.length; i++) {
             double subtotal = precios[i] * cantidades[i];
             double impuesto = calcularIVA(subtotal, tipos[i]);
-            double resultadoItem = subtotal + impuesto;
-
-            resultadoItem = aplicarDescuento(resultadoItem, cantidades[i]);
+            double resultadoItem = aplicarDescuento(subtotal + impuesto, cantidades[i]);
             
             System.out.println("Item " + i + ": " + resultadoItem);
             totalFinal += resultadoItem;
         }
-        System.out.println("TOTAL FINAL: " + totalFinal);
-
-        Scanner sc = new Scanner(System.in);
-        System.out.println("¿Confirmar pedido? (s/n)");
-        String respuesta = sc.nextLine();
-        if (respuesta.equals("s")) {
-            System.out.println("Guardando...");
-        }
+        return totalFinal;
     }
 
     public static double calcularIVA(double subtotal, int tipo) {
-        if (tipo == 1) {
-            return subtotal * 0.04;
-        } else if (tipo == 2) {
-            return subtotal * 0.21;
-        }
+        if (tipo == 1) return subtotal * 0.04;
+        if (tipo == 2) return subtotal * 0.21;
         return 0;
     }
 
     public static double aplicarDescuento(double totalItem, int cantidad) {
-        if (cantidad > 3) {
-            return totalItem * 0.9;
+        return (cantidad > 3) ? totalItem * 0.9 : totalItem;
+    }
+
+    public static void gestionarConfirmacion() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("¿Confirmar pedido? (s/n)");
+        if (sc.nextLine().equalsIgnoreCase("s")) {
+            System.out.println("Guardando...");
         }
-        return totalItem;
     }
 }
