02c18dbbaaaf56254adc97f4f8fa0efd556c99e5
prueba24122005-ai <prueba24122005@gmail.com>
2026-03-17 19:41:39 +0100

    Update README.java
    Hacer variable el tipo de producto

--- README.java
+++ README.java
@@ -3,18 +3,22 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         // Datos con nombres para mejor comprension
+        Scanner sc = new Scanner(System.in);
         String[] producto = {"Manzanas", "Auriculares", "Pan", "Tablet"};
         double[] precio = {10.5, 20.0, 5.75, 100.0};
         int[] cantidad = {2, 1, 5, 1};
-        int[] tipo = {1, 2, 1, 2}; // 1: Alimento, 2: Electronica
-        double total = 0;
+        int tipo = 0;
 
+        double total = 0;
+        System.out.println("Dime el tipo de producto que es [1]Alimentos [2]Electronica");
+        tipo = sc.nextInt();
+        
         System.out.println("\n--- FACTURA ---");
         for (int i = 0; i < precio.length; i++) {
             double subtotal = precio[i] * cantidad[i];
-            double impuesto = (tipo[i] == 1) ? subtotal * 0.04 : subtotal * 0.21;
+            double impuesto = (tipo == 1) ? subtotal * 0.04 : subtotal * 0.21;
             double totalItem = subtotal + impuesto;
-
+            
             // Descuento si compra mas de 3
             if (cantidad[i] > 3) {
                 totalItem = totalItem * 0.9; // 10% descuento
@@ -29,7 +33,7 @@ public class Main {
         System.out.printf("TOTAL: %.2f€%n", total);
         
         // Confirmacion simple
-        Scanner sc = new Scanner(System.in);
+        
         System.out.print("Confirmar? (s/n): ");
         if (sc.nextLine().equals("s")) {
             System.out.println("Guardado");
